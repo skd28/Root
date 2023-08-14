36f21127deef87917c74b349fbd96ec7b9b55b70
skd28 <shivajaldwivedi@gmail.com>
2023-08-14 14:52:43 +0530

    update

--- src/app/componets/Growing_Values.js
+++ src/app/componets/Growing_Values.js
@@ -30,7 +30,7 @@ function Growing_Values() {
 
     return (
         <>
-            <div className="bg-custom-blue py-16 px-24 ">
+            <div className="bg-custom-blue py-12 px-24 ">
                 <div className="text-white text-4xl ">
                     <h1 >{services && services[0].attributes.Name}</h1>
                 </div>
@@ -52,7 +52,7 @@ function Growing_Values() {
                                 <div className="rotate-180 text-white text-3xl" style={{ writingMode: "vertical-rl", width: "40" }}>
                                     {services && services[0].attributes.Title}
                                 </div>
-                                <div className="w-5 h-5 pt-24 ms-3">
+                                <div className="w-5 h-5 pt-12 ms-3">
                                     <div className=" bg-white w-5 h-5 rounded">
                                     </div>
                                     {/* <input type="radio" className="w-5 h-5 pb-10" /> */}
@@ -64,7 +64,7 @@ function Growing_Values() {
                                         position: "relative",
                                         top: '10px'
                                     }}>
-                                        <img src={services[0].attributes.GIFlink} alt="Image Description" className="items-center w-80 h-80" />
+                                        <img src={services[0].attributes.GIFlink} alt="Image Description" className="items-center w-80 h-64" />
                                     </div>
                                 )}
                             </div>
@@ -73,9 +73,9 @@ function Growing_Values() {
 
 
                         <div className="flex">
-                            <div className="ms-5 -mt-24" style={{ backgroundImage: 'url("/Image/Vector 5.png")', backgroundSize: "cover", height: "500px", width: "630px" }}>
+                            <div className="ms-5 -mt-20" style={{ backgroundImage: 'url("/Image/Vector 5.png")', backgroundSize: "cover", height: "400px", width: "600px" }}>
 
-                                <div className="ms-10  pt-72">
+                                <div className="ms-10  pt-52">
                                     <div style={{ width: "70%", height: "150px", float: "left" }}>
                                         <p className="w-96 text-sm">
                                             {services && <p className="text-white text-justify ms-5">{services[0].attributes.Description}</p>}
@@ -83,7 +83,7 @@ function Growing_Values() {
                                     </div>
                                 </div>
 
-                                <div className="mt-14">
+                                <div className="mt-10">
                                     <div className="bg-custom-blue" style={{ width: "10%", height: "100px", float: "right" }}>
                                         <p className="rotate-180 text-white text-3xl justify-end" style={{ writingMode: "vertical-rl", width: "40" }}>
                                             {services && services[1].attributes.Title}
@@ -112,15 +112,15 @@ function Growing_Values() {
                         }}
                     >
                         {services && (
-                            <p className="text-white text-justify  text-sm mt-8 px-7 pr-20">
+                            <p className="text-white text-justify  text-sm mt-8 px-10 pr-20">
                                 {services && services[1].attributes.Description}
                             </p>
                         )}
 
-                        <p className="mt-48 ms-7">
+                        <p className="mt-28 ms-7">
                             {services && (
                                 <img src={services[1].attributes.GIFlink} alt="Image Description"
-                                    className="items-center w-96 h-96 " />
+                                    className="items-center w-80 h-80 " />
                             )}
                         </p>
                     </div>
