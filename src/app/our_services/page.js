4dbc9eb5a13d98dcdb78a49f0a792f98fcc9016f
skd28 <shivajaldwivedi@gmail.com>
2023-10-04 17:21:19 +0530

    update

--- src/app/our_services/page.js
+++ src/app/our_services/page.js
@@ -175,9 +175,9 @@ function Our_Service() {
 
             </div>
 
-            <svm >
-
-            </svm>
+            <svg height="100" width="100">
+                <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+            </svg>
 
 
 
